f2846dbba17a229ba40e009a03e242a73573a512
MBrock29 <mbrock19@gmail.com>
2020-08-25 13:54:15 +0100

    updated code

--- src/Reducer.js
+++ src/Reducer.js
@@ -3,7 +3,10 @@ export const initialState = {
     user: null, 
 }
 
-function reducer(state, action) {
+export const getBasketTotal = (basket) => 
+basket?.reduce((amount, item) => item.price + amount, 0)
+
+const reducer = (state, action) => {
     console.log(action)
     switch(action.type) {
         case "ADD_TO_BASKET":
@@ -12,7 +15,17 @@ function reducer(state, action) {
                 basket: [...state.basket, action.item]
             }
             case "REMOVE_FROM_BASKET":
-            return { state } 
+            let newBasket = [...state.basket]
+            const index = state.basket.findIndex((basketItem) => basketItem.id === action.id)
+
+            if (index >= 0) {
+                newBasket.splice(index, 1) 
+            } else {
+                console.warn(
+                    `Cannot remove product (id: ${action.id}) as it's not in basket`
+                )
+            }
+            return { ...state, basket: newBasket}
             default:
                 return state
     }
